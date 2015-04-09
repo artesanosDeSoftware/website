f0d4ee90916ac9c4aa1344b2a9d479912753769b
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-04-08 23:39:17 -0500

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf public && hugo --theme=hyde-x --verbose && rsync -avzhe ssh --delete --delete-after ./public deployer@198.58.120.79:/www/artesanos.de
+rm -rf software
+mkdir software
+rm -rf public && hugo --theme=hyde-x --verbose && mv public/* software && mv software public/ && cp foo/index.html public && rsync -avzhe ssh --delete --delete-after ./public deployer@198.58.120.79:/www/artesanos.de
