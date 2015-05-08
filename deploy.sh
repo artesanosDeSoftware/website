a0a3c761f97e3607bcf9b48c1e404b74a908625e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-05-07 22:44:55 -0500

    added robots.txt

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 rm -rf software
 mkdir software
-rm -rf public && hugo --verbose && mv public/* software && mv software public/ && cp foo/index.html public && rsync -avzhe ssh --delete --delete-after ./public deployer@198.58.120.79:/www/artesanos.de
+rm -rf public && hugo --verbose && mv public/* software && mv software public/ && cp foo/* public && rsync -avzhe ssh --delete --delete-after ./public deployer@198.58.120.79:/www/artesanos.de
