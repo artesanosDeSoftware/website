ca32c9edf5e6a3959801b852551e31feec938a60
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-07-12 22:25:10 -0500

    Containerized

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 VER=$(grep "version" website.properties|cut -d'=' -f2)  && \
   docker build -t artesanos/website:$VER \
     -t registry.circulosiete.com/library/artesanos/website:$VER . && \
-  #git release $VER && \
-  #docker push artesanos/website:$VER && \
-  #docker push registry.circulosiete.com/library/artesanos/website:$VER && \
+  git release $VER && \
+  docker push artesanos/website:$VER && \
+  docker push registry.circulosiete.com/library/artesanos/website:$VER && \
   echo "Done"
