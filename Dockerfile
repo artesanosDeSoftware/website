6554834fd4f291dbb50658fbfc26f2cc1bf064ad
Domingo Suarez Torres <domingo.suarez@gmail.com>
2020-07-12 22:24:17 -0500

    Containerized

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY ./site ./
 RUN sh -c './build.sh'
 
 # build runtime image
-FROM nginx:1.15.2-alpine
+FROM nginx:1.19.1-alpine
 
-COPY --from=build-env /app/public/ /www/artesanos.de/public/
-COPY conf/nginx/* /etc/nginx/conf.d
+COPY --from=build-env /app/public/ /usr/share/nginx/html
--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-VER=$(grep "version" website.properties|cut -d'=' -f2)  && docker build -t artesanos/website:$VER . && docker push artesanos/website:$VER #&& kubectl set image deployment/website website=artesanos/website:$VER'
+VER=$(grep "version" website.properties|cut -d'=' -f2)  && \
+  docker build -t artesanos/website:$VER \
+    -t registry.circulosiete.com/library/artesanos/website:$VER . && \
+  #git release $VER && \
+  #docker push artesanos/website:$VER && \
+  #docker push registry.circulosiete.com/library/artesanos/website:$VER && \
+  echo "Done"
